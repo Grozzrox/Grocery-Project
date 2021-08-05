dfc66e0983604ce3b6359e449480463add386fa6
Andrew <grozzy09@gmail.com>
2021-08-04 20:16:59 -0400

    Shopping Cart and Login Modals

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
     $('.header').height($(window).height());
 });
+
+const loginButton = document.getElementById('loginButton');
+const shoppingCart = document.getElementById('shoppingCart');
+
+loginButton.addEventListener("click", function() {
+    $('#loginModal').modal('show');
+})
+
+shoppingCart.addEventListener("click", function() {
+    $('#cartModal').modal('show');
+})
