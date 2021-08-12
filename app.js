d65b7d2c1b8708663fa98c7f9425e0f0d3678342
Andrew <grozzy09@gmail.com>
2021-08-11 21:02:48 -0400

    Plans section and carousel activity

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
 const loginButton = document.getElementById('loginButton');
 const shoppingCart = document.getElementById('shoppingCart');
+const carouselItems = document.querySelectorAll('.carousel-item');
 
 loginButton.addEventListener("click", function() {
     $('#loginModal').modal('show');
@@ -12,3 +13,11 @@ loginButton.addEventListener("click", function() {
 shoppingCart.addEventListener("click", function() {
     $('#cartModal').modal('show');
 })
+
+for (let i = 0; i < carouselItems.length; i++) {
+    carouselItems[i].addEventListener("click", () => $('.carousel').carousel('pause') );
+}
+
+$('.carousel').carousel({
+    interval: 5000
+});
