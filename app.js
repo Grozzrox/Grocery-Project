ea66e48df0f967630074c2b30993af7080b8cef2
Andrew <grozzy09@gmail.com>
2021-08-20 17:42:41 -0400

    item count increment/decrement

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const carouselItems = document.querySelectorAll('.carousel-item');
 const foodCards = document.querySelectorAll('.foodCard');
 const foodModals = document.querySelectorAll('.foodModal');
 const produceModal = document.querySelector('#produceModal');
+const itemFields = document.querySelectorAll('.itemField');
+const addBtns = document.querySelectorAll('#button-addon2');
+const minusBtns = document.querySelectorAll('#button-addon1');
+
+let itemCount = 0;
 
 loginButton.addEventListener("click", function() {
     $('#loginModal').modal('show');
@@ -28,5 +33,19 @@ $('.carousel').carousel({
 for (let i = 0; i < foodCards.length; i++) {
     foodCards[i].addEventListener("click", function() {
         $(foodModals[i]).modal('show');
+    });
+}
+
+for (let i = 0; i < itemFields.length; i++) {
+    addBtns[i].addEventListener("click", function() {
+        itemCount++;
+        itemFields[i].value = itemCount;
+    });
+}
+
+for (let i = 0; i < itemFields.length; i++) {
+    minusBtns[i].addEventListener("click", function() {
+        itemCount --;
+        itemFields[i].value = itemCount;
     });
 }
